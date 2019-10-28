a616abc6cedf654d83c87cb7509b11567a5a7341
Jordan Richlen <jmrichlen@gmail.com>
2019-10-28 00:06:46 -0500

    fix tests

--- test/client.spec.js
+++ test/client.spec.js
@@ -8,58 +8,6 @@ describe("Client", () => {
     const component = renderer.create(<Client />);
 
     // then
-    expect(component.toJSON()).toMatchInlineSnapshot(`
-      <div>
-        <header
-          className="MuiPaper-root MuiPaper-elevation4 MuiAppBar-root MuiAppBar-positionStatic MuiAppBar-colorPrimary"
-        >
-          <div
-            className="MuiToolbar-root MuiToolbar-regular MuiToolbar-gutters"
-          >
-            <button
-              className="MuiButtonBase-root MuiIconButton-root"
-              disabled={false}
-              onBlur={[Function]}
-              onDragLeave={[Function]}
-              onFocus={[Function]}
-              onKeyDown={[Function]}
-              onKeyUp={[Function]}
-              onMouseDown={[Function]}
-              onMouseLeave={[Function]}
-              onMouseUp={[Function]}
-              onTouchEnd={[Function]}
-              onTouchMove={[Function]}
-              onTouchStart={[Function]}
-              tabIndex={0}
-              type="button"
-            >
-              <span
-                className="MuiIconButton-label"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="MuiSvgIcon-root"
-                  focusable="false"
-                  role="presentation"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
-                  />
-                </svg>
-              </span>
-              <span
-                className="MuiTouchRipple-root"
-              />
-            </button>
-            <h6
-              className="MuiTypography-root MuiTypography-h6"
-            >
-              NPPF Banquet
-            </h6>
-          </div>
-        </header>
-      </div>
-    `);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
